01218d14bca0da785d0301c8dab296653586e47c
Viktoryia Tamazian <vikatamazian@gmail.com>
2021-10-17 13:21:43 +0300

    fix mapping, add lombok

--- src/main/java/com/trainee/moviestore/controller/UserController.java
+++ src/main/java/com/trainee/moviestore/controller/UserController.java
@@ -2,44 +2,41 @@ package com.trainee.moviestore.controller;
 
 import com.trainee.moviestore.model.User;
 import com.trainee.moviestore.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/user")
+    @GetMapping("/")
     private List<User> show() {
         return userService.getAll();
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     private User getUser(@PathVariable("id") long id) {
         return userService.findById(id);
     }
 
-    @PostMapping("/user/new")
+    @PostMapping("/")
     private User saveUser(@RequestBody User user) {
         userService.create(user);
         return user;
     }
 
-    @PutMapping("/user/update")
+    @PutMapping("/")
     private User update(@RequestBody User user) {
         userService.update(user);
         return user;
     }
 
-    @DeleteMapping("/user/{id}")
+    @DeleteMapping("/{id}")
     private void deleteUser(@PathVariable("id") long id) {
         userService.delete(id);
     }
