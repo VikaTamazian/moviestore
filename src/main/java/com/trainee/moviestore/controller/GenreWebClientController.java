647b0bd95968fa2d0942ffe52457dbb5eab455c3
AnastasiyaIlkevich <sirin199@yandex.ru>
2021-11-06 19:55:14 +0300

    MVS-31 Implement search functionality (MVS-57 Get movies.)

--- src/main/java/com/trainee/moviestore/controller/GenreWebClientController.java
+++ src/main/java/com/trainee/moviestore/controller/GenreWebClientController.java
@@ -1,20 +1,24 @@
 package com.trainee.moviestore.controller;
 
+import com.trainee.moviestore.dto.GenresDto;
+import com.trainee.moviestore.dto.MovieDto;
+import com.trainee.moviestore.dto.MoviesResponseTmdb;
 import com.trainee.moviestore.service.TheMoviedbService;
-import com.trainee.moviestore.test.GenresDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/genres/tmdb")
+@RequestMapping("/search")
 public class GenreWebClientController {
 
     final TheMoviedbService tmdbService;
@@ -23,20 +27,38 @@ public class GenreWebClientController {
 
     @PostConstruct
     private void setUpWebClient() {
-        try {
-            webClient = WebClient.create(tmdbService.createGenreUrlBuilder());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+        webClient = WebClient.create();
     }
 
-    @GetMapping()
-    public Flux<GenresDto> getAllGenreWebClient() {
+    @GetMapping("/genres")
+    public Flux<GenresDto> getAllGenreWebClient() throws URISyntaxException {
 
         return webClient
                 .get()
+                .uri(tmdbService.createGenreSearchUrlBuilder())
                 .retrieve()
                 .bodyToFlux(GenresDto.class);
 
+    }
+
+    @GetMapping("/movies/{title}")
+    public Flux<MoviesResponseTmdb> getAllMoviesWebClient(@PathVariable("title") String title) throws URISyntaxException {
+
+        return webClient
+                .get()
+                .uri(tmdbService.createMoviesSearchByTitleUrlBuilder(title))
+                .retrieve()
+                .bodyToFlux(MoviesResponseTmdb.class);
+
+    }
+
+    @GetMapping("/movie/{id}")
+    public Mono<MovieDto> getMovieWebClient(@PathVariable("id") Long id) throws URISyntaxException {
+
+        return webClient
+                .get()
+                .uri(tmdbService.createMovieSearchByIdUrlBuilder(id))
+                .retrieve()
+                .bodyToMono(MovieDto.class);
     }
 }
--- src/main/java/com/trainee/moviestore/service/TheMoviedbService.java
+++ src/main/java/com/trainee/moviestore/service/TheMoviedbService.java
@@ -14,14 +14,41 @@ public class TheMoviedbService {
     private String tmdbApiKey;
     @Value("${tmdb.api.path.get.genres}")
     private String tmdbGenres;
+    @Value("${tmdb.api.path.search.titele}")
+    private String tmdbMovies;
+    @Value("${tmdb.api.path.search.id}")
+    private String tmdbMovieById;
+    @Value("${tmdb.language}")
+    private String tmdbLanguage;
+
 
     private final String API_KEY = "api_key";
+    private final String LANGUAGE = "language";
+    private final String QUERY = "query";
 
-    public String createGenreUrlBuilder() throws URISyntaxException {
+    public String createGenreSearchUrlBuilder() throws URISyntaxException {
 
         URIBuilder uriBuilder = new URIBuilder(tmdbApiUrl + tmdbGenres);
         uriBuilder.addParameter(API_KEY, tmdbApiKey);
         return uriBuilder.build().toString();
+    }
 
+    public String createMoviesSearchByTitleUrlBuilder(String title) throws URISyntaxException {
+
+        URIBuilder uriBuilder = new URIBuilder(tmdbApiUrl + tmdbMovies);
+        uriBuilder.addParameter(API_KEY, tmdbApiKey);
+        uriBuilder.addParameter(LANGUAGE, tmdbLanguage);
+        uriBuilder.addParameter(QUERY, title);
+        System.out.println(uriBuilder.build().toString());
+        return uriBuilder.build().toString();
+    }
+
+    public String createMovieSearchByIdUrlBuilder(Long id) throws URISyntaxException {
+
+        URIBuilder uriBuilder = new URIBuilder(tmdbApiUrl + tmdbMovieById + id);
+        uriBuilder.addParameter(API_KEY, tmdbApiKey);
+        uriBuilder.addParameter(LANGUAGE, tmdbLanguage);
+        System.out.println(uriBuilder.build().toString());
+        return uriBuilder.build().toString();
     }
 }
