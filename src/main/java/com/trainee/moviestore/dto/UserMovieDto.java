7e514382fb767bf641063ca2b1c2125928d040e0
Viktoryia Tamazian <vikatamazian@gmail.com>
2021-11-24 21:26:28 +0300

    add Status

--- src/main/java/com/trainee/moviestore/dto/UserMovieDto.java
+++ src/main/java/com/trainee/moviestore/dto/UserMovieDto.java
@@ -1,6 +1,7 @@
 package com.trainee.moviestore.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.trainee.moviestore.model.Status;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,6 @@ public class UserMovieDto {
     private long id;
     private String description;
     private int rating;
+    private Status status;
 
 }
